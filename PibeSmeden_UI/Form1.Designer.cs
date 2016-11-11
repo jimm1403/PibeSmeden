4614ced10b6ce9a0fac69838635840fbfd8346f9
Liljeroth <chri894u@edu.eal.dk>
2016-11-11 08:35:31 +0100

    Tilføjet FormAdd

--- PibeSmeden_UI/Form1.Designer.cs
+++ PibeSmeden_UI/Form1.Designer.cs
@@ -30,16 +30,8 @@
         {
             this.ChooseProduct = new System.Windows.Forms.ComboBox();
             this.txtChooseProduct = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
             this.txtChosenProduct = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // ChooseProduct
@@ -75,130 +67,36 @@
             this.txtChooseProduct.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.txtChooseProduct.WordWrap = false;
             // 
+            // btnAdd
+            // 
+            this.btnAdd.ForeColor = System.Drawing.Color.Black;
+            this.btnAdd.Location = new System.Drawing.Point(35, 124);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Tilføj";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
             // txtChosenProduct
             // 
             this.txtChosenProduct.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtChosenProduct.Cursor = System.Windows.Forms.Cursors.Default;
-            this.txtChosenProduct.Location = new System.Drawing.Point(196, 63);
+            this.txtChosenProduct.Location = new System.Drawing.Point(35, 153);
             this.txtChosenProduct.Name = "txtChosenProduct";
             this.txtChosenProduct.ReadOnly = true;
             this.txtChosenProduct.Size = new System.Drawing.Size(126, 13);
-            this.txtChosenProduct.TabIndex = 2;
+            this.txtChosenProduct.TabIndex = 13;
             this.txtChosenProduct.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(43, 89);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(97, 20);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(43, 125);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Tilføj";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(165, 88);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 5;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(286, 88);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 6;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
-            // 
-            // textBox4
-            // 
-            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox4.Cursor = System.Windows.Forms.Cursors.Default;
-            this.textBox4.Location = new System.Drawing.Point(43, 70);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(82, 13);
-            this.textBox4.TabIndex = 7;
-            this.textBox4.TabStop = false;
-            this.textBox4.Text = "Mærke";
-            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox4.WordWrap = false;
-            // 
-            // textBox5
-            // 
-            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox5.Cursor = System.Windows.Forms.Cursors.Default;
-            this.textBox5.Location = new System.Drawing.Point(165, 70);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.ReadOnly = true;
-            this.textBox5.Size = new System.Drawing.Size(82, 13);
-            this.textBox5.TabIndex = 8;
-            this.textBox5.TabStop = false;
-            this.textBox5.Text = "Type";
-            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox5.WordWrap = false;
-            // 
-            // textBox6
-            // 
-            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox6.Cursor = System.Windows.Forms.Cursors.Default;
-            this.textBox6.Location = new System.Drawing.Point(286, 69);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.ReadOnly = true;
-            this.textBox6.Size = new System.Drawing.Size(82, 13);
-            this.textBox6.TabIndex = 9;
-            this.textBox6.TabStop = false;
-            this.textBox6.Text = "Pris";
-            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox6.WordWrap = false;
-            // 
-            // textBox7
-            // 
-            this.textBox7.Location = new System.Drawing.Point(407, 88);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 20);
-            this.textBox7.TabIndex = 10;
-            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
-            // 
-            // textBox8
-            // 
-            this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox8.Cursor = System.Windows.Forms.Cursors.Default;
-            this.textBox8.Location = new System.Drawing.Point(407, 70);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.ReadOnly = true;
-            this.textBox8.Size = new System.Drawing.Size(82, 13);
-            this.textBox8.TabIndex = 11;
-            this.textBox8.TabStop = false;
-            this.textBox8.Text = "Styk/Vægt";
-            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox8.WordWrap = false;
+            this.txtChosenProduct.TextChanged += new System.EventHandler(this.txtChosenProduct_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(514, 281);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.txtChosenProduct);
+            this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtChooseProduct);
             this.Controls.Add(this.ChooseProduct);
             this.ForeColor = System.Drawing.Color.Transparent;
@@ -216,16 +114,8 @@
 
         private System.Windows.Forms.ComboBox ChooseProduct;
         private System.Windows.Forms.TextBox txtChooseProduct;
+        private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.TextBox txtChosenProduct;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
     }
 }
 
--- PibeSmeden_UI/Form1.cs
+++ PibeSmeden_UI/Form1.cs
@@ -1,22 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using ClassLibrary;
 
 namespace PibeSmeden_UI
 {
     public partial class Form1 : Form
     {
-        string itemType;
-        string brand;
-        string brandType;
-        string price;
-        string pieces;
+        public string itemType;
+
         public Form1()
         {
             InitializeComponent();
@@ -27,36 +17,15 @@ namespace PibeSmeden_UI
             itemType = ChooseProduct.Text;
         }
 
-        private void textBox1_TextChanged_1(object sender, EventArgs e)
-        {
-
-            TextBox ToEmne = (TextBox)sender;
-            brand = ToEmne.Text;
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            TextBox ToEmne = (TextBox)sender;
-            brandType = ToEmne.Text;
-        }
-
-        private void textBox3_TextChanged(object sender, EventArgs e)
-        {
-            TextBox ToEmne = (TextBox)sender;
-            price = ToEmne.Text;
-        }
-
-        private void textBox7_TextChanged(object sender, EventArgs e)
-        {
-            TextBox ToEmne = (TextBox)sender;
-            pieces = ToEmne.Text;
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            Item myItem = new Item(itemType, brand, brandType, double.Parse(price), int.Parse(pieces));
+            FormAdd myForm = new FormAdd();
+            myForm.ShowDialog();
+        }
 
-            
+        public void txtChosenProduct_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
